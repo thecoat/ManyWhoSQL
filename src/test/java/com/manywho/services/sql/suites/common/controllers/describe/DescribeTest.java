4e08b3a5c8eaf169bdb4dc6811b74f507cda92a5
Jose Collazzi <jose.collazzi@manywho.com>
2017-01-11 16:28:57 +0000

    #9 remove description test for alias for sqlserver

--- src/test/java/com/manywho/services/sql/suites/common/controllers/describe/DescribeTest.java
+++ src/test/java/com/manywho/services/sql/suites/common/controllers/describe/DescribeTest.java
@@ -56,35 +56,6 @@ public class DescribeTest extends ServiceFunctionalTest {
         );
     }
 
-    @Test
-    public void testDescribeWithAliases() throws Exception {
-        DbConfigurationTest.setPropertiesIfNotInitialized("postgresql");
-
-        String sql = "CREATE TABLE " + scapeTableName("country") + "(" +
-                "id integer NOT NULL," +
-                "name character varying(255)," +
-                "description character varying(1024)," +
-                "CONSTRAINT country_id_pk PRIMARY KEY (id)" +
-                ");";
-
-        String aliasName = "COMMENT ON COLUMN " + scapeTableName("country") + ".name IS '{{ManyWhoName:The Name}}';";
-        //ignored at the moment
-        String aliasTable = "COMMENT ON TABLE " + scapeTableName("country") + " IS '{{ManyWhoName:The Country}}';";
-
-        try (Connection connection = getSql2o().open()) {
-            connection.createQuery(sql).executeUpdate();
-            connection.createQuery(aliasName).executeUpdate();
-            connection.createQuery(aliasTable).executeUpdate();
-        }
-
-        DefaultApiRequest.describeServiceRequestAndAssertion("/metadata",
-                "suites/common/describe/with-alias-types/metadata1-request.json",
-                configurationParameters(),
-                "suites/common/describe/with-alias-types/metadata1-response.json",
-                dispatcher
-        );
-    }
-
     @Test(expected = Exception.class)
     public void testDescribeWithTypesButNotPort() throws Exception {
         DbConfigurationTest.setPropertiesIfNotInitialized("postgresql");
