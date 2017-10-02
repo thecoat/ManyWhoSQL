de41c573c617246f6ee97c687e9c8456a4394db8
jose <jose.collazzi@manywho.com>
2017-10-02 20:16:43 +0100

    issue #18 error when updating autoincrement

--- src/test/java/com/manywho/services/sql/suites/postgresql/data/AutoIncrementTest.java
+++ src/test/java/com/manywho/services/sql/suites/postgresql/data/AutoIncrementTest.java
@@ -32,6 +32,32 @@ public class AutoIncrementTest extends ServiceFunctionalTest {
         );
     }
 
+    @Test
+    public void testUpdateAutoincrement() throws Exception {
+        DbConfigurationTest.setPropertiesIfNotInitialized("postgresql");
+        try (Connection connection = getSql2o().open()) {
+            String sql = "CREATE TABLE " + scapeTableName("country") + "(" +
+                    "name character varying(255)," +
+                    "description character varying(1024), " +
+                    "id SERIAL NOT NULL," +
+                    "CONSTRAINT country_id_pk PRIMARY KEY (id)" +
+                    ");";
+            connection.createQuery(sql).executeUpdate();
+        }
+
+        try (Connection connection = getSql2o().open()) {
+            String sql = "INSERT INTO " + scapeTableName("country") + "( name, description) VALUES ( 'Uruguay', 'It is a nice country');";
+            connection.createQuery(sql).executeUpdate();
+        }
+
+        DefaultApiRequest.saveDataRequestAndAssertion("/data",
+                "suites/common/data/save/update/update-request.json",
+                configurationParameters(),
+                "suites/common/data/save/update/update-response.json",
+                dispatcher
+        );
+    }
+
     @After
     public void cleanDatabaseAfterEachTest() {
         try (Connection connection = getSql2o().open()) {
